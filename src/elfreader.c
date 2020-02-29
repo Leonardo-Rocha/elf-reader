c370db238571479ea74822794f78dde6933494b7
leo-rocha <leonardocar.dm@gmail.com>
2020-02-29 11:30:13 -0400

    Add src folder with sample file.

---
+++ src/elfreader.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define BUFFER_SIZE 200
+
+unsigned char buffer[BUFFER_SIZE];
+
+int main(int argc, char argv[])
+{
+    printf("Trying to config the debugger...");
+    return 0;
+}
